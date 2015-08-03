c9c802e00405bc890434e80a4f7d446ebc26d932
Kunal Varma <kunalvarma05@gmail.com>
2015-08-03 16:10:26 +0530

    creative commons

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -16,3 +16,11 @@
 	<!-- End: Container -->
 </div>
 <!-- End: Footer -->
+<div class="creative-common">
+	<div class="container">
+		<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+			<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" />
+		</a>
+		<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/StillImage" property="dct:title" rel="dct:type">Digital India Hackathon Logo</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://kunalvarma.in" property="cc:attributionName" rel="cc:attributionURL">Kunal Varma</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+	</div>
+</div>
--- app/views/splash.blade.php
+++ app/views/splash.blade.php
@@ -673,24 +673,7 @@
 				<!-- End: Container -->
 			</div>
 			<!-- End: Contact Section -->
-			<!-- Footer -->
-			<div class="footer">
-				<!-- Container -->
-				<div class="container">
-					<!-- Row -->
-					<div class="row">
-						<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 pull-right">
-							<a href="http://kunalvarma.in" class="btn btn-block btn-link">Designed by Kunal Varma.</a>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-							<a href="index.htm" class="copyright btn-block btn btn-link">2015 - All rights reserved.</a>
-						</div>
-					</div>
-					<!-- End: Row -->
-				</div>
-				<!-- End: Container -->
-			</div>
-			<!-- End: Footer -->
+			@include('partials.footer')
 		</div>
 		<!-- End: Main -->
 	</div>
